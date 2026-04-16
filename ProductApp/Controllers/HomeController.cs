8c8cc2cb5a3f64358da16b6486b0f393b211e9f1
RevolutionAdmin <achyut.kendre@gmail.com>
2026-04-16 08:56:58 +0530

    Add project files.

---
+++ ProductApp/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+namespace ProductApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var res=Product.GetProducts();
+            return View(res);
+        }
+    }
+}
---
+++ ProductApp/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿namespace ProductApp.Models
+{
+    public class Product
+    {
+        public Int64 ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string MfgName { get; set; }
+        public decimal Price { get; set; }
+        public static List<Product> GetProducts()
+        {
+          List<Product> products = new List<Product>();
+            products.Add(new Product
+            {
+                ProductID = 1,
+                ProductName = "Mouse",
+                MfgName = "Logitech",
+                Price = 550
+            });
+            products.Add(new Product
+            {
+                ProductID = 2,
+                ProductName = "Keyboard",
+                MfgName = "Logitech",
+                Price = 550
+            });
+            products.Add(new Product
+            {
+                ProductID = 3,
+                ProductName = "Monitor",
+                MfgName = "Lg",
+                Price = 5500
+            });
+            return products;
+        }
+    }
+}
